f6e87d2084035ec9b590496a7ff8b85a064c5778
Yusuke Takeda <61795460+ytakeda-paleo@users.noreply.github.com>
2024-05-10 15:34:03 +0900

    Add files via upload

---
+++ actget.py
@@ -0,0 +1,229 @@
+import os,sys,requests
+from xml.etree import ElementTree as ET
+import argparse
+import getpass
+import zipfile
+from concurrent.futures import ThreadPoolExecutor
+from time import time
+
+# Replace 'url' with your WebDAV server URL
+source = 'https://dc-act.spring8.or.jp/remote.php/dav/files/'
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user', type=str, default=None, help='userID for aCT download website',required=True) 
+    parser.add_argument('--proposal', type=str, default=None, help='Proposal No., e.g., 2024AXXXX',required=True)
+    parser.add_argument('--sampleid', type=str, default=None, nargs='*', help='Sample ID (if known) , 10-digits number')
+    parser.add_argument('--zip', type=str, default=None, nargs='*', 
+        choices=['rh', 'rh_222', 'rh_444', 'ro', 'ro_222', 'ro_444', 'all','allro','allrh','all222','all444'],
+        required=True)
+    parser.add_argument('--output', type=str, default=None, help='where to save (Default = /UserData/YOUR_USER_ID/PROPOSAL_NO/SAMPLE_ID)')
+    parser.add_argument('--nounzip', action='store_true', default=False, help='use this option when you do not want to extract zip files')
+    
+    args = parser.parse_args()
+
+    if args.zip == ["all"]:
+        args.zip = ["ro_444", "rh_444", "ro_222", "rh_222", "ro", "rh"]
+    elif args.zip == ["allrh"]:
+        args.zip = ["rh_444", "rh_222", "rh"]
+    elif args.zip == ["allro"]:
+        args.zip = ["ro_444", "ro_222", "ro"]
+    elif args.zip == "all222":
+        args.zip = ["ro_222", "rh_222"]
+    elif args.zip == ["all444"]:
+        args.zip = ["ro_444", "rh_444"]
+    else:
+        pass
+
+    print("\n")
+    args.pw = getpass.getpass()
+
+
+
+    def urlcheck(url, stage):
+        # Set up the headers to ask for directory contents (depth can be 0, 1, or "infinity")
+        headers = {
+            'Depth': '1',
+            'Content-Type': 'application/xml'
+                    }
+        # Make the PROPFIND request
+        response = requests.request("PROPFIND", url, auth=(args.user, args.pw), headers=headers)
+        # Check if the request was successful
+        if response.status_code != 207:  # 207 Multi-Status means success in WebDAV
+            print(f"access failured. Check {stage}")
+            sys.exit()
+        else:
+            return response
+    
+    def get_sample_id(response):
+        # Parse the XML response
+        tree = ET.fromstring(response.content)
+        namespaces = {'d': 'DAV:'}  # Adjust the namespace if necessary
+        print("\n")
+        print(f"{args.proposal} Searching...")
+        sample_id_list = []
+        # Find all 'href' elements - they contain the directory names
+        num = 0
+        for href in tree.findall('.//d:href', namespaces):
+            dirname = os.path.basename(os.path.dirname(href.text))
+            if dirname != args.proposal:
+                sample_id_list.append(dirname)
+                num = num + 1
+
+        if num != 0:
+            print(f"{num} data found")
+            print("\n")
+            return sample_id_list
+        else:
+            print("no data found")
+            print("\n")
+            sys.exit()
+        
+    
+    def downloadzip(proposal,sampleid,inputzip,outputzip):
+        start = time()
+        print(f"Downloading: {inputzip}")
+        # Make a GET request to the WebDAV server to download the file
+        response = requests.get(inputzip, auth=(args.user, args.pw), stream=True)
+        total_size = int(response.headers.get('content-length', 0))
+        downloaded = 0
+        last_reported_percent = 0
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Write the content of the response to a local file
+            #output = str(output) + "/" + str(args.zip) + ".zip"
+            with open(outputzip, 'wb') as file:
+                # file.write(response.content)
+                for data in response.iter_content(1024*1024):
+                    file.write(data)
+                    downloaded += len(data)
+                    current_percent = (downloaded / total_size) * 100
+                    # Update the progress bar only if the increase is significant
+                    if current_percent - last_reported_percent >= 1:
+                        print_progress_bar(current_percent)
+                        last_reported_percent = current_percent
+                #print("\n")
+            
+            elapsed = time() - start
+            filesize = round(total_size / 1024 ** 2, 2)
+            speed = round(filesize / elapsed)
+            print(f"zip downloaded successfully!: {outputzip} ({round(elapsed)} sec. elapsed, speed = {speed} MB/sec)")
+        else:
+            print(f"Failed to download file. Status code: {response.status_code}")
+            print("\n")
+
+    def print_progress_bar(percent_complete):
+        bar_length = 50
+        filled_length = int(bar_length * percent_complete // 100)
+        bar = '█' * filled_length + '-' * (bar_length - filled_length)
+        print(f"\rProgress: |{bar}| {percent_complete:.2f}%", end='\r')
+
+    def extract_zip(zip_path, extract_to):
+        # Open the zip file
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            # Get the list of file names
+            file_names = zip_ref.namelist()
+            total_files = len(file_names)  # Total files to extract
+            update_interval = max(1, total_files // 100)  # Update every 1% of the total files
+
+            # Extract files one by one
+            for i, file in enumerate(file_names, start=1):
+                # Extract the file
+                zip_ref.extract(file, extract_to)
+                # Only update progress after a certain number of extracts or at the end
+                if i % update_interval == 0 or i == total_files:
+                    progress = (i / total_files) * 100  # Calculate progress percentage
+                    sys.stdout.write(f"\rExtracting... {progress:.2f}%")
+                    sys.stdout.flush()
+            
+    ## sampleIDリスト取得
+    if args.sampleid == None:
+        url = str(source) + str(args.user) + "/" + str(args.proposal)
+        proposal_response = urlcheck(url, "--user or Password or --proposal")
+        sample_id_list = get_sample_id(proposal_response)
+    else: 
+        sample_id_list = args.sampleid
+        url = str(source) + str(args.user) + "/" + str(args.proposal)  
+        urlcheck(url, "--user or Password or --proposal")
+        for sampleid in sample_id_list:    
+            url = str(source) + str(args.user) + "/" + str(args.proposal) + "/" + str(sampleid)
+            urlcheck(url, "--sampleid")
+
+    ## 保存先の整合性確認
+    if args.output == None:
+        outputpath = "/UserData/" + str(args.user)
+        if os.path.exists(outputpath) is False:
+            outputpath = os.getcwd()
+    else:
+        outputpath = args.output
+
+    os.makedirs(os.path.dirname(outputpath), exist_ok=True)
+    print(f"\nZip will be donwloaded to {outputpath}")
+
+    ## 入出力リスト作成
+    inoutlist = []
+    for sampleid in sample_id_list:
+            for ziptype in args.zip:
+                inputzip = str(source) + str(args.user) + "/" + str(args.proposal) + "/" + str(sampleid) + "/" + str(ziptype) + ".zip"
+                outputzip = outputpath + "/" + str(args.proposal) + "/" + str(sampleid) + "/" + str(ziptype) + ".zip"
+                inout = [args.proposal, sampleid, inputzip, outputzip, ziptype]
+                inoutlist.append(inout)
+
+    ## 軽いものからダウンロードするようにリストを並び替える
+    d_order = {"ro_444" : 0, "rh_444" : 1, "ro_222" : 2, "rh_222" : 3, "ro" : 4, "rh" : 5}
+    inoutlist = sorted(inoutlist, key=lambda x: d_order[x[4]])
+    print("Smaller zip (4x4x4 and ro) will be first served. PRESS Ctrl + C for cancel")
+    print("\n")
+
+    ## リストを順番にダウンロード(,展開,zip削除)
+    for data in inoutlist:
+        os.makedirs(os.path.dirname(data[3]), exist_ok=True)
+        downloadzip(data[0],data[1],data[2],data[3])
+        
+        if args.nounzip == False:
+            start = time()
+            zipdir = os.path.dirname(data[3])
+            os.makedirs(zipdir, exist_ok=True)
+            extract_zip(data[3], zipdir)
+            os.remove(data[3])
+            if data[4] == "ro" or data[4] == "rh": ## roとrhは二段階の展開
+                print(f"\nExtracting {data[4]}-1,-2,-3,-4.zip in parallel (takes some time)")
+                zipdir = os.path.dirname(data[3]) + "/" + data[4]
+
+                def rorhzip(path):
+                    with zipfile.ZipFile(path) as zf:
+                        zf.extractall(zipdir)
+                    os.remove(path)
+                ziplist = [zipdir+ "-" + str(i) + ".zip" for i in range(1,5)]
+                os.makedirs(zipdir, exist_ok=True)
+                with ThreadPoolExecutor(max_workers=4) as executor:
+                    executor.map(rorhzip,ziplist)
+
+                # マルチスレッド（４並列）での展開が失敗する場合は逐次処理                
+                # extract_zip(zipdir + "-1.zip", zipdir)
+                # print(f"\n{data[4]}-1.zip extracted") 
+                # os.remove(zipdir+ "-1.zip")
+                # extract_zip(zipdir + "-2.zip", zipdir)
+                # print(f"\n{data[4]}-2.zip extracted")
+                # os.remove(zipdir+ "-2.zip") 
+                # extract_zip(zipdir + "-3.zip", zipdir)
+                # print(f"\n{data[4]}-3.zip extracted") 
+                # os.remove(zipdir+ "-3.zip")
+                # extract_zip(zipdir + "-4.zip", zipdir)
+                # print(f"\n{data[4]}-4.zip extracted") 
+                # os.remove(zipdir+ "-4.zip")
+
+                elapsed = round(time() - start)
+                print(f"Extraction complete. ({elapsed} sec.)")   
+                print("\n")
+
+            else:
+                elapsed = round(time() - start)
+                print(f"\nExtraction complete. ({elapsed} sec.)")  
+                print("\n")  
+        else:
+            pass
+    
+if __name__ == '__main__':
+    main()
---
+++ actinfo.py
@@ -0,0 +1,263 @@
+import os,sys,requests
+from xml.etree import ElementTree as ET
+import argparse
+import getpass
+import csv
+
+# Replace 'url' with your WebDAV server URL
+source = 'https://dc-act.spring8.or.jp/remote.php/dav/files/'
+
+# initColors, findLargestElement, createMatrix, makeRows, createWrappingRows, createRowUnderFields, printRowsInTable, printTable are from:
+# https://github.com/SuperMaZingCoder/TableIt
+# Copyright 2021 SuperMaZingCoder
+# Under MIT lisence
+def initColors():
+    os.system("cls")
+
+def findLargestElement(rows, cols, lengthArray, matrix):
+    # Loop through each row
+    for i in range(rows):  
+        # Loop through each column
+        for j in range(cols):
+            lengthArray.append(len(str(matrix[i][j])))
+    # Sort the length matrix so that we can find the element with the longest length
+    lengthArray.sort()
+    # Store that length
+    largestElementLength = lengthArray[-1]
+
+    return largestElementLength
+
+
+def createMatrix(rows, cols, matrixToWorkOn, matrix):
+    # Loop through each row
+    for i in range(rows):    
+        # Append a row to matrixToWorkOn for each row in the matrix passed in
+        matrixToWorkOn.append([])
+        # Loop through each column
+        for j in range(cols):
+            # Add a each column of the current row (in string form) to matrixToWorkOn
+            matrixToWorkOn[i].append(str(matrix[i][j]))
+
+def makeRows(rows, cols, largestElementLength, rowLength, matrixToWorkOn, finalTable, color):
+    # Loop through each row
+    for i in range(rows):
+        # Initialize the row that will we work on currently as a blank string
+        currentRow = ""
+        # Loop trhough each column
+        for j in range(cols):
+            # If we are using colors then do the same thing but as without (below)
+            if ((color != None) and (j == 0 or i == 0)):
+                # Only add color if it is in the first column or first row
+                currentEl = " " + "\033[38;2;" + str(color[0]) + ";" + str(color[1]) + ";" + str(color[2]) +"m" + matrixToWorkOn[i][j] + "\033[0m"
+            # If we are not using colors (or j != 0 or i != 0) just add a space and the element that should be in that position to a variable which will store the current element to work on
+            else:
+                currentEl = " " + matrixToWorkOn[i][j]
+
+            # If the raw element is less than the largest length of a raw element (raw element is just the unformatted element passed in)
+            if (largestElementLength != len(matrixToWorkOn[i][j])):
+                # If we are using colors then add the amount of spaces that is equal to the difference of the largest element length and the current element (minus the length that is added for the color)
+                # * The plus two here comes from the one space we would normally need and the fact that we need to account for a space that tbe current element already has
+                if (color != None):
+                    if (j == 0 or i == 0):
+                        currentEl = currentEl + " " * (largestElementLength - (len(currentEl) - len("\033[38;2;" + str(color[0]) + ";" + str(color[1]) + ";" + str(color[2]) + "m" + "\033[0m")) + 2) + "|"
+                    # If it is not the first column or first row than it doesn't need to subtract the color length
+                    else:
+                        currentEl = currentEl + " " * (largestElementLength - len(currentEl) + 2) + "|"
+                # If we are not using color just do the same thing as above when we were using colors for when the row or column is not the first each time
+                else:
+                    currentEl = currentEl + " " * (largestElementLength - len(currentEl) + 2) + "|"
+            # If the raw element length us equal to the largest length of a raw element then we don't need to add extra spaces
+            else:
+                currentEl = currentEl + " " + "|"
+            # Now add the current element to the row that we are working on
+            currentRow += currentEl
+        # When the entire row that we were working on is done add it as a row to the final table that we will print
+        finalTable.append("|" + currentRow)
+    # If we are using color then the length of each row (each row will end up being the same length) equals to the length of the last row (again each row will end up being the same length) minus the length the color will inevitably add if we are using colors
+    if (color != None):
+        rowLength = len(currentRow) - len("\033[38;2;" + str(color[0]) + ";" + str(color[1]) + ";" + str(color[2]) + "m" + "\033[0m")
+    # Otherwise (we are not using colors) the length of each row will be equal to the length of the last row (each row will end up being the same length)
+    else:
+        rowLength = len(currentRow)  
+    
+    return rowLength
+
+def createWrappingRows(rowLength, finalTable):
+    # Here we deal with the rows that will go on the top and bottom of the table (look like -> +--------------+), we start by initializing an empty string
+    wrappingRows = ""
+    # Then for the length of each row minus one (have to account for the plus that comes at the end, not minus two because rowLength doesn't include the | at the beginning) we add a -
+    for i in range(rowLength - 1):
+        wrappingRows += "-"
+    # Add a plus at the beginning
+    wrappingRows = "+" + wrappingRows
+    # Add a plus at the end
+    wrappingRows += "+"
+
+     # Add the two wrapping rows
+    finalTable.insert(0, wrappingRows)
+    finalTable.append(wrappingRows)
+
+def createRowUnderFields(largestElementLength, cols, finalTable):
+    # Initialize the row that will be created 
+    rowUnderFields = ""
+    # Loop through each column
+    for j in range(cols):
+        # For each column add a plus
+        currentElUnderField = "+" 
+        # Then add an amount of -'s equal to the length of largest raw element and add 2 for the 2 spaces that will be either side the element
+        currentElUnderField = currentElUnderField + "-" * (largestElementLength + 2)
+        # Then add the current element (there will be one for each column) to the final row that will be under the fields
+        rowUnderFields += currentElUnderField
+    # Add a final plus at the end of the row
+    rowUnderFields += "+"
+    # Insert this row under the first row
+    finalTable.insert(2, rowUnderFields)
+
+def printRowsInTable(finalTable):
+    # For each row - print it
+    for row in finalTable:
+        print(row)
+
+def printTable(matrix, useFieldNames=False, color=None):
+    # Rows equal amount of lists inside greater list
+    rows = len(matrix)
+    # Cols equal amount of elements inside each list
+    cols = len(matrix[0])
+    # This is the array to sort the length of each element
+    lengthArray = []
+    # This is the variable to store the vakye of the largest length of any element
+    largestElementLength = None
+    #This is the variable that will store the length of each row
+    rowLength = None
+    # This is the matrix that we will work with throughout this program (main difference between matrix passed in and this matrix is that the matrix that is passed in doesn't always have elements which are all strings)
+    matrixToWorkOn = []
+    #This the list in which each row will be one of the final table to be printed
+    finalTable = []
+
+    largestElementLength = findLargestElement(rows, cols, lengthArray, matrix)
+    createMatrix(rows, cols, matrixToWorkOn, matrix)
+    rowLength = makeRows(rows, cols, largestElementLength, rowLength, matrixToWorkOn, finalTable, color)
+    createWrappingRows(rowLength, finalTable)
+    if (useFieldNames):
+        createRowUnderFields(largestElementLength, cols, finalTable)
+    printRowsInTable(finalTable)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user', type=str, default=None, help='userID for aCT download website',required=True) 
+    parser.add_argument('--proposal', type=str, default=None, help='Proposal No., e.g., 2024AXXXX',required=True)
+    parser.add_argument('--sampleid', type=str, default=None, help='Sample ID, 10-digits number (optional)')
+    parser.add_argument('--samplename', type=str, default=None, help='Sample Name (optional)')
+    parser.add_argument('--pixelsize', type=float, default=None, help='Pixel size (without unit, optional)')
+    parser.add_argument('--exposure', type=float, default=None, help='Exposure (without unit, optional)')
+    parser.add_argument('--projections', type=int, default=None, help='Number of projections (optional)')
+    parser.add_argument('--xray', type=float, default=None, help='X-Ray Energy (without unit, optional)')
+    parser.add_argument('--csv', action='store_true', default=False, help='output csv file (optional)')
+    args = parser.parse_args()
+
+    print("\n")
+    args.pw = getpass.getpass()
+    
+    def urlcheck(url, stage):
+        # Set up the headers to ask for directory contents (depth can be 0, 1, or "infinity")
+        headers = {
+            'Depth': '1',
+            'Content-Type': 'application/xml'
+                    }
+        # Make the PROPFIND request
+        response = requests.request("PROPFIND", url, auth=(args.user, args.pw), headers=headers)
+        # Check if the request was successful
+        if response.status_code != 207:  # 207 Multi-Status means success in WebDAV
+            print(f"access failured. Check {stage}")
+            sys.exit()
+        else:
+            return response
+
+    def getinfo(response):
+        # Parse the XML response
+        tree = ET.fromstring(response.content)
+        namespaces = {'d': 'DAV:'}  # Adjust the namespace if necessary
+        print("\n")
+        print(f"{args.proposal} Searching...")
+        header = [["Sample ID", "Sample Name", "Pixel Size", "Exposure", "Projections", "X-ray"]]
+        # Find all 'href' elements - they contain the directory names
+        num = 0
+
+        keys = [args.sampleid, args.samplename, args.pixelsize, args.exposure, args.projections, args.xray]
+
+        if keys[0] == None:
+            for href in tree.findall('.//d:href', namespaces):
+                dirname = os.path.basename(os.path.dirname(href.text))
+                if dirname != args.proposal:
+                    HEADERurl = str(source) + str(args.user) + "/" + str(args.proposal) + "/" + str(dirname) +  "/HEADER.md"
+                    HEADERdata = requests.get(HEADERurl, auth=(args.user, args.pw)).content
+                    HEADERdata = HEADERdata.decode('utf8')
+                    HEADERdata = HEADERdata.splitlines()
+                    HEADERdata = HEADERdata[2].split('|')
+
+                    #if args.samplename == None and args.pixelsize == None and args.exposure == None and args.projections == None and args.xray == None:
+                    if all(x is None for x in keys) is True:
+                        header.append([HEADERdata[2],HEADERdata[3],HEADERdata[4],HEADERdata[5],HEADERdata[6],HEADERdata[7]])
+                        num = num + 1
+                    else:
+                        #if HEADERdata[3] == args.samplename or float(HEADERdata[4].split()[0]) == args.pixelsize or float(HEADERdata[5].split()[0]) == args.exposure or int(HEADERdata[6]) == args.projections or float(HEADERdata[7].split()[0]) == args.xray:
+                        if keys[1] is None or keys[1] == HEADERdata[3]:
+                            if keys[2] is None or keys[2] == float(HEADERdata[4].split()[0]):
+                                if keys[3] is None or keys[3] == float(HEADERdata[5].split()[0]):
+                                    if keys[4] is None or keys[4] == int(HEADERdata[6]):
+                                        if keys[5] is None or keys[5] == float(HEADERdata[7].split()[0]):
+                                            header.append([HEADERdata[2],HEADERdata[3],HEADERdata[4],HEADERdata[5],HEADERdata[6],HEADERdata[7]])
+                                            num = num + 1 
+                                        else:
+                                            pass
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            else:
+                                pass
+                        else:
+                            pass
+                else:
+                    pass
+        else:
+            HEADERurl = str(source) + str(args.user) + "/" + str(args.proposal) + "/" + str(args.sampleid) +  "/HEADER.md"
+            HEADERdata = requests.get(HEADERurl, auth=(args.user, args.pw)).content
+            HEADERdata = HEADERdata.decode('utf8')
+            HEADERdata = HEADERdata.splitlines()
+            HEADERdata = HEADERdata[2].split('|') 
+            header.append([HEADERdata[2],HEADERdata[3],HEADERdata[4],HEADERdata[5],HEADERdata[6],HEADERdata[7]])
+            num = num + 1           
+
+        if num != 0:
+            print(f"{num} data found")
+            printTable(header, useFieldNames=True)
+            print("\n")
+            if args.csv == True:
+                csvpath = f'{args.proposal}.csv'
+                # Writing to a CSV file
+                with open(csvpath, 'w', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerows(header)
+                print(f"csv file saved as: {os.getcwd()}/{csvpath}")
+                print("\n")
+        else:
+            print("no match")
+            print("\n")
+    
+    if args.sampleid != None:
+        url = str(source) + str(args.user) + "/" + str(args.proposal) + "/" + str(args.sampleid)
+        proposal_response = urlcheck(url, "--sampleid")
+        getinfo(proposal_response)
+
+    elif args.sampleid == None:
+        url = str(source) + str(args.user) + "/" + str(args.proposal)
+        proposal_response = urlcheck(url, "--user or Password or --proposal")
+        getinfo(proposal_response)
+    
+    else:
+        pass
+    
+if __name__ == '__main__':
+    main()
